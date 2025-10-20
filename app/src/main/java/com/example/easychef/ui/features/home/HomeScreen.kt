9f11c95ba49479baee595a28edbd2bb42a10b067
TheStoneYura <julesyparedes@gmail.com>
2025-10-20 12:53:44 -0700

    homeScreen update

--- app/src/main/java/com/example/easychef/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/easychef/ui/features/home/HomeScreen.kt
@@ -2,21 +2,42 @@ package com.example.easychef.ui.features.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.easychef.ui.components.Button
 
 @Composable
 fun HomeScreen(id: String) {
-    // TODO: Make Home Screen and retrieve Supabase user data by id
+    //TODO: Hook up Supabase user data using 'id'
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(text = id, onClick = {})
+        Text(
+            text = "Welcome back, $id!",
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Aiden's custom button component
+        Button(text = "Go to Pantry", onClick = {
+            //TODO: Navigate to pantry screen later
+        })
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(id = "DemoUser")
+}
